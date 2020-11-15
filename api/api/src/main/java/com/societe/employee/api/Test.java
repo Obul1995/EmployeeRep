f3b8e295041b41cf3707b2f1319b81e6a98fc6a1
Obul1995 <pobulreddy039@gmail.com>
2020-11-15 18:20:19 +0530

    Create Test.java
    test push

---
+++ api/api/src/main/java/com/societe/employee/api/Test.java
@@ -0,0 +1,14 @@
+package com.societe.employee.api;
+
+public class Test {
+public String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+}
